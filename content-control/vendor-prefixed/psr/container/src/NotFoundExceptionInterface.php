43f271827cf005b9acba625d94f6fc1f4f15fae2
Sean Murphy <sean@iamseanmurphy.com>
2025-05-09 06:18:32 +0000

    2.6.4

--- content-control/vendor-prefixed/psr/container/src/NotFoundExceptionInterface.php
+++ content-control/vendor-prefixed/psr/container/src/NotFoundExceptionInterface.php
@@ -2,7 +2,7 @@
 /**
  * @license MIT
  *
- * Modified by code-atlantic on 08-May-2025 using {@see https://github.com/BrianHenryIE/strauss}.
+ * Modified by code-atlantic on 09-May-2025 using {@see https://github.com/BrianHenryIE/strauss}.
  */
 
 namespace ContentControl\Vendor\Psr\Container;
--- content-control/vendor/composer/installed.php
+++ content-control/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'code-atlantic/content-control',
-        'pretty_version' => 'v2.6.3',
-        'version' => '2.6.3.0',
-        'reference' => 'ab20fd84f3fc22199e8bb0df331f2af58d97315f',
+        'pretty_version' => 'v2.6.4',
+        'version' => '2.6.4.0',
+        'reference' => '8f0564cc719c204181e47eac20dd805f0a6dc27b',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'code-atlantic/content-control' => array(
-            'pretty_version' => 'v2.6.3',
-            'version' => '2.6.3.0',
-            'reference' => 'ab20fd84f3fc22199e8bb0df331f2af58d97315f',
+            'pretty_version' => 'v2.6.4',
+            'version' => '2.6.4.0',
+            'reference' => '8f0564cc719c204181e47eac20dd805f0a6dc27b',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
